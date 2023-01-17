9706f2bddd10eae9af0ac5ea60d1f3669adc124d
DeepaKovvada <118762067+DeepaKovvada@users.noreply.github.com>
2023-01-16 20:56:28 -0500

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,26 +1,93 @@
-provider "azurerm" {
-  features {}
 
-  subscription_id = "9e04de8c-d344-4b9d-9da1-606e614944df"
-  client_id       = "35b0341a-cbf8-4e79-ad34-e9b98bc2d7db"
-  client_secret   = "LHM8Q~ipunK2HaBWgbBwTVAd-wwOAWhjCCsg6b5T"
-  tenant_id       = "b4edbf0a-eecd-4b80-8486-28231a271593"
+locals  {
+    prefix = "${var.organisation}-${var.environment}-${var.application}-${var.tier}"
 }
+resource "azurerm_resource_group" "rg1" {
+  name     = "${local.prefix}-${var.rgname}"
+  location =  var.location
+  tags = var.tagging
+}
+resource "azurerm_virtual_network" "vnett1" {
+  name                = "${local.prefix}-${var.vnetname}"
+  address_space       = var.address_space
+  location            = azurerm_resource_group.rg1.location
+  resource_group_name = azurerm_resource_group.rg1.name
+}
+resource "azurerm_subnet" "subnet1" {
+  name                 = "${local.prefix}-${var.subnetname}"
+   address_prefixes     = var.address_prefixes
+   resource_group_name  = azurerm_resource_group.rg1.name
+  virtual_network_name = azurerm_virtual_network.vnett1.name
+}
+resource "azurerm_network_interface" "nic" {
+  name                = "${local.prefix}-${var.nicname}"
+  location            = azurerm_resource_group.rg1.location
+  resource_group_name = azurerm_resource_group.rg1.name
 
-resource "azurerm_resource_group" "rg"{
-    name = "diskrg"
-    location = "west US"
-    tags = {
-        env = "test"
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet1.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = azurerm_public_ip.pipp.id
+  }
+    }
+resource "azurerm_public_ip" "pipp" {
+  name                = "${var.vnetname}-${var.pipname}"
+  resource_group_name = azurerm_resource_group.rg1.name
+  location            = azurerm_resource_group.rg1.location
+  allocation_method   = "Static"
+
+  tags = var.tagging
+  }
+resource "azurerm_network_security_group" "nsg" {
+  name                = "${local.prefix}-${var.nsg}"
+  location            = azurerm_resource_group.rg1.location
+  resource_group_name = azurerm_resource_group.rg1.name
+
+  security_rule {
+    name                       = "test123"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = 3389
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
   }
 }
-resource "azurerm_managed_disk" "rg"{
-    name = "disk1"
-    resource_group_name = azurerm_resource_group.rg.name
-    location = azurerm_resource_group.rg.location
-     storage_account_type = "Standard_LRS"
-  create_option        = "Empty"
-  disk_size_gb         = "1"
+resource "azurerm_network_interface_security_group_association" "example" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+resource "azurerm_virtual_machine" "main" {
+  name                  = "${local.prefix}-${var.VMname}"
+  location              = azurerm_resource_group.rg1.location
+  resource_group_name   = azurerm_resource_group.rg1.name
+  network_interface_ids = [azurerm_network_interface.nic.id]
+  vm_size               = var.vm_size
+os_profile_windows_config{}
+
+ delete_os_disk_on_termination = true
+
+   delete_data_disks_on_termination = true
+ storage_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
+    version   = "latest"
+  }
+  storage_os_disk {
+    name              = "myosdisk1"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  =  var.computer_name
+    admin_username =  var.admin_username
+    admin_password =  var.admin_password
+  }
 
   tags = {
     environment = "staging"
