7a39525929b621fb5d008560c034ead56d645370
DeepaKovvada <118762067+DeepaKovvada@users.noreply.github.com>
2023-01-16 20:55:10 -0500

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,27 +1,28 @@
+provider "azurerm" {
+  features {}
 
-resource "azurerm_resource_group" "example" {
-  name     = "vnetrg"
-  location = "West Europe"
+  subscription_id = "9e04de8c-d344-4b9d-9da1-606e614944df"
+  client_id       = "35b0341a-cbf8-4e79-ad34-e9b98bc2d7db"
+  client_secret   = "LHM8Q~ipunK2HaBWgbBwTVAd-wwOAWhjCCsg6b5T"
+  tenant_id       = "b4edbf0a-eecd-4b80-8486-28231a271593"
 }
 
-
-resource "azurerm_virtual_network" "example" {
-  name                = "vnet1"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  address_space       = ["10.0.0.0/16"]
-  subnet {
-    name           = "subnet1"
-    address_prefix = "10.0.1.0/24"
+resource "azurerm_resource_group" "rg"{
+    name = "diskrg"
+    location = "west US"
+    tags = {
+        env = "test"
   }
-
-  subnet {
-    name           = "subnet2"
-    address_prefix = "10.0.2.0/24"
-    
-  }
+}
+resource "azurerm_managed_disk" "rg"{
+    name = "disk1"
+    resource_group_name = azurerm_resource_group.rg.name
+    location = azurerm_resource_group.rg.location
+     storage_account_type = "Standard_LRS"
+  create_option        = "Empty"
+  disk_size_gb         = "1"
 
   tags = {
-    environment = "Production"
+    environment = "staging"
   }
 }
